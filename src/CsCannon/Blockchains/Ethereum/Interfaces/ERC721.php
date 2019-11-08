39d85157f5dc2032c4ef832fcb14a1327a9c1e3c
shaban shaame <shaban@everdreamsoft.com>
2019-11-08 19:54:19 +0100

    adding ABI on the interface

--- src/CsCannon/Blockchains/Ethereum/Interfaces/ERC721.php
+++ src/CsCannon/Blockchains/Ethereum/Interfaces/ERC721.php
@@ -88,5 +88,572 @@ class ERC721 extends EthereumContractStandard
         return $return ;
     }
 
+    public function getInterfaceAbi()
+    {
+
+        $strJsonFileContents = "[
+    {
+      \"constant\": true,
+      \"inputs\": [
+        {
+          \"name\": \"interfaceId\",
+          \"type\": \"bytes4\"
+        }
+      ],
+      \"name\": \"supportsInterface\",
+      \"outputs\": [
+        {
+          \"name\": \"\",
+          \"type\": \"bool\"
+        }
+      ],
+      \"payable\": false,
+      \"stateMutability\": \"view\",
+      \"type\": \"function\"
+    },
+    {
+      \"constant\": true,
+      \"inputs\": [],
+      \"name\": \"name\",
+      \"outputs\": [
+        {
+          \"name\": \"\",
+          \"type\": \"string\"
+        }
+      ],
+      \"payable\": false,
+      \"stateMutability\": \"view\",
+      \"type\": \"function\"
+    },
+    {
+      \"constant\": true,
+      \"inputs\": [
+        {
+          \"name\": \"tokenId\",
+          \"type\": \"uint256\"
+        }
+      ],
+      \"name\": \"getApproved\",
+      \"outputs\": [
+        {
+          \"name\": \"\",
+          \"type\": \"address\"
+        }
+      ],
+      \"payable\": false,
+      \"stateMutability\": \"view\",
+      \"type\": \"function\"
+    },
+    {
+      \"constant\": false,
+      \"inputs\": [
+        {
+          \"name\": \"to\",
+          \"type\": \"address\"
+        },
+        {
+          \"name\": \"tokenId\",
+          \"type\": \"uint256\"
+        }
+      ],
+      \"name\": \"approve\",
+      \"outputs\": [],
+      \"payable\": false,
+      \"stateMutability\": \"nonpayable\",
+      \"type\": \"function\"
+    },
+    {
+      \"constant\": true,
+      \"inputs\": [],
+      \"name\": \"totalSupply\",
+      \"outputs\": [
+        {
+          \"name\": \"\",
+          \"type\": \"uint256\"
+        }
+      ],
+      \"payable\": false,
+      \"stateMutability\": \"view\",
+      \"type\": \"function\"
+    },
+    {
+      \"constant\": false,
+      \"inputs\": [
+        {
+          \"name\": \"from\",
+          \"type\": \"address\"
+        },
+        {
+          \"name\": \"to\",
+          \"type\": \"address\"
+        },
+        {
+          \"name\": \"tokenId\",
+          \"type\": \"uint256\"
+        }
+      ],
+      \"name\": \"transferFrom\",
+      \"outputs\": [],
+      \"payable\": false,
+      \"stateMutability\": \"nonpayable\",
+      \"type\": \"function\"
+    },
+    {
+      \"constant\": true,
+      \"inputs\": [
+        {
+          \"name\": \"owner\",
+          \"type\": \"address\"
+        },
+        {
+          \"name\": \"index\",
+          \"type\": \"uint256\"
+        }
+      ],
+      \"name\": \"tokenOfOwnerByIndex\",
+      \"outputs\": [
+        {
+          \"name\": \"\",
+          \"type\": \"uint256\"
+        }
+      ],
+      \"payable\": false,
+      \"stateMutability\": \"view\",
+      \"type\": \"function\"
+    },
+    {
+      \"constant\": false,
+      \"inputs\": [
+        {
+          \"name\": \"from\",
+          \"type\": \"address\"
+        },
+        {
+          \"name\": \"to\",
+          \"type\": \"address\"
+        },
+        {
+          \"name\": \"tokenId\",
+          \"type\": \"uint256\"
+        }
+      ],
+      \"name\": \"safeTransferFrom\",
+      \"outputs\": [],
+      \"payable\": false,
+      \"stateMutability\": \"nonpayable\",
+      \"type\": \"function\"
+    },
+    {
+      \"constant\": true,
+      \"inputs\": [
+        {
+          \"name\": \"index\",
+          \"type\": \"uint256\"
+        }
+      ],
+      \"name\": \"tokenByIndex\",
+      \"outputs\": [
+        {
+          \"name\": \"\",
+          \"type\": \"uint256\"
+        }
+      ],
+      \"payable\": false,
+      \"stateMutability\": \"view\",
+      \"type\": \"function\"
+    },
+    {
+      \"constant\": true,
+      \"inputs\": [
+        {
+          \"name\": \"tokenId\",
+          \"type\": \"uint256\"
+        }
+      ],
+      \"name\": \"ownerOf\",
+      \"outputs\": [
+        {
+          \"name\": \"\",
+          \"type\": \"address\"
+        }
+      ],
+      \"payable\": false,
+      \"stateMutability\": \"view\",
+      \"type\": \"function\"
+    },
+    {
+      \"constant\": true,
+      \"inputs\": [
+        {
+          \"name\": \"owner\",
+          \"type\": \"address\"
+        }
+      ],
+      \"name\": \"balanceOf\",
+      \"outputs\": [
+        {
+          \"name\": \"\",
+          \"type\": \"uint256\"
+        }
+      ],
+      \"payable\": false,
+      \"stateMutability\": \"view\",
+      \"type\": \"function\"
+    },
+    {
+      \"constant\": false,
+      \"inputs\": [],
+      \"name\": \"renounceOwnership\",
+      \"outputs\": [],
+      \"payable\": false,
+      \"stateMutability\": \"nonpayable\",
+      \"type\": \"function\"
+    },
+    {
+      \"constant\": true,
+      \"inputs\": [],
+      \"name\": \"owner\",
+      \"outputs\": [
+        {
+          \"name\": \"\",
+          \"type\": \"address\"
+        }
+      ],
+      \"payable\": false,
+      \"stateMutability\": \"view\",
+      \"type\": \"function\"
+    },
+    {
+      \"constant\": true,
+      \"inputs\": [],
+      \"name\": \"isOwner\",
+      \"outputs\": [
+        {
+          \"name\": \"\",
+          \"type\": \"bool\"
+        }
+      ],
+      \"payable\": false,
+      \"stateMutability\": \"view\",
+      \"type\": \"function\"
+    },
+    {
+      \"constant\": true,
+      \"inputs\": [],
+      \"name\": \"symbol\",
+      \"outputs\": [
+        {
+          \"name\": \"\",
+          \"type\": \"string\"
+        }
+      ],
+      \"payable\": false,
+      \"stateMutability\": \"view\",
+      \"type\": \"function\"
+    },
+    {
+      \"constant\": false,
+      \"inputs\": [
+        {
+          \"name\": \"to\",
+          \"type\": \"address\"
+        },
+        {
+          \"name\": \"approved\",
+          \"type\": \"bool\"
+        }
+      ],
+      \"name\": \"setApprovalForAll\",
+      \"outputs\": [],
+      \"payable\": false,
+      \"stateMutability\": \"nonpayable\",
+      \"type\": \"function\"
+    },
+    {
+      \"constant\": false,
+      \"inputs\": [
+        {
+          \"name\": \"from\",
+          \"type\": \"address\"
+        },
+        {
+          \"name\": \"to\",
+          \"type\": \"address\"
+        },
+        {
+          \"name\": \"tokenId\",
+          \"type\": \"uint256\"
+        },
+        {
+          \"name\": \"_data\",
+          \"type\": \"bytes\"
+        }
+      ],
+      \"name\": \"safeTransferFrom\",
+      \"outputs\": [],
+      \"payable\": false,
+      \"stateMutability\": \"nonpayable\",
+      \"type\": \"function\"
+    },
+    {
+      \"constant\": true,
+      \"inputs\": [
+        {
+          \"name\": \"tokenId\",
+          \"type\": \"uint256\"
+        }
+      ],
+      \"name\": \"tokenURI\",
+      \"outputs\": [
+        {
+          \"name\": \"\",
+          \"type\": \"string\"
+        }
+      ],
+      \"payable\": false,
+      \"stateMutability\": \"view\",
+      \"type\": \"function\"
+    },
+    {
+      \"constant\": true,
+      \"inputs\": [
+        {
+          \"name\": \"owner\",
+          \"type\": \"address\"
+        },
+        {
+          \"name\": \"operator\",
+          \"type\": \"address\"
+        }
+      ],
+      \"name\": \"isApprovedForAll\",
+      \"outputs\": [
+        {
+          \"name\": \"\",
+          \"type\": \"bool\"
+        }
+      ],
+      \"payable\": false,
+      \"stateMutability\": \"view\",
+      \"type\": \"function\"
+    },
+    {
+      \"constant\": false,
+      \"inputs\": [
+        {
+          \"name\": \"newOwner\",
+          \"type\": \"address\"
+        }
+      ],
+      \"name\": \"transferOwnership\",
+      \"outputs\": [],
+      \"payable\": false,
+      \"stateMutability\": \"nonpayable\",
+      \"type\": \"function\"
+    },
+    {
+      \"inputs\": [],
+      \"payable\": false,
+      \"stateMutability\": \"nonpayable\",
+      \"type\": \"constructor\"
+    },
+    {
+      \"anonymous\": false,
+      \"inputs\": [
+        {
+          \"indexed\": true,
+          \"name\": \"from\",
+          \"type\": \"address\"
+        },
+        {
+          \"indexed\": true,
+          \"name\": \"to\",
+          \"type\": \"address\"
+        },
+        {
+          \"indexed\": true,
+          \"name\": \"tokenId\",
+          \"type\": \"uint256\"
+        }
+      ],
+      \"name\": \"Transfer\",
+      \"type\": \"event\"
+    },
+    {
+      \"anonymous\": false,
+      \"inputs\": [
+        {
+          \"indexed\": true,
+          \"name\": \"owner\",
+          \"type\": \"address\"
+        },
+        {
+          \"indexed\": true,
+          \"name\": \"approved\",
+          \"type\": \"address\"
+        },
+        {
+          \"indexed\": true,
+          \"name\": \"tokenId\",
+          \"type\": \"uint256\"
+        }
+      ],
+      \"name\": \"Approval\",
+      \"type\": \"event\"
+    },
+    {
+      \"anonymous\": false,
+      \"inputs\": [
+        {
+          \"indexed\": true,
+          \"name\": \"owner\",
+          \"type\": \"address\"
+        },
+        {
+          \"indexed\": true,
+          \"name\": \"operator\",
+          \"type\": \"address\"
+        },
+        {
+          \"indexed\": false,
+          \"name\": \"approved\",
+          \"type\": \"bool\"
+        }
+      ],
+      \"name\": \"ApprovalForAll\",
+      \"type\": \"event\"
+    },
+    {
+      \"anonymous\": false,
+      \"inputs\": [
+        {
+          \"indexed\": true,
+          \"name\": \"previousOwner\",
+          \"type\": \"address\"
+        },
+        {
+          \"indexed\": true,
+          \"name\": \"newOwner\",
+          \"type\": \"address\"
+        }
+      ],
+      \"name\": \"OwnershipTransferred\",
+      \"type\": \"event\"
+    },
+    {
+      \"constant\": false,
+      \"inputs\": [
+        {
+          \"name\": \"userAddress\",
+          \"type\": \"address\"
+        },
+        {
+          \"name\": \"uidHash\",
+          \"type\": \"bytes32\"
+        },
+        {
+          \"name\": \"googleHash\",
+          \"type\": \"bytes32\"
+        },
+        {
+          \"name\": \"nonce\",
+          \"type\": \"uint64\"
+        }
+      ],
+      \"name\": \"registerUser\",
+      \"outputs\": [],
+      \"payable\": false,
+      \"stateMutability\": \"nonpayable\",
+      \"type\": \"function\"
+    },
+    {
+      \"constant\": false,
+      \"inputs\": [
+        {
+          \"name\": \"userAddress\",
+          \"type\": \"address\"
+        },
+        {
+          \"name\": \"newLevel\",
+          \"type\": \"uint64\"
+        }
+      ],
+      \"name\": \"updateUserLevel\",
+      \"outputs\": [],
+      \"payable\": false,
+      \"stateMutability\": \"nonpayable\",
+      \"type\": \"function\"
+    },
+    {
+      \"constant\": false,
+      \"inputs\": [
+        {
+          \"name\": \"userAddress\",
+          \"type\": \"address\"
+        },
+        {
+          \"name\": \"newCertificationLevel\",
+          \"type\": \"uint64\"
+        }
+      ],
+      \"name\": \"updateUserCertificationLevel\",
+      \"outputs\": [],
+      \"payable\": false,
+      \"stateMutability\": \"nonpayable\",
+      \"type\": \"function\"
+    },
+    {
+      \"constant\": true,
+      \"inputs\": [
+        {
+          \"name\": \"userAddress\",
+          \"type\": \"address\"
+        }
+      ],
+      \"name\": \"getUser\",
+      \"outputs\": [
+        {
+          \"name\": \"nonce\",
+          \"type\": \"uint64\"
+        },
+        {
+          \"name\": \"level\",
+          \"type\": \"uint64\"
+        },
+        {
+          \"name\": \"certificationLevel\",
+          \"type\": \"uint64\"
+        },
+        {
+          \"name\": \"uidHash\",
+          \"type\": \"bytes32\"
+        },
+        {
+          \"name\": \"googleHash\",
+          \"type\": \"bytes32\"
+        }
+      ],
+      \"payable\": false,
+      \"stateMutability\": \"view\",
+      \"type\": \"function\"
+    },
+    {
+      \"constant\": false,
+      \"inputs\": [
+        {
+          \"name\": \"userAddress\",
+          \"type\": \"address\"
+        }
+      ],
+      \"name\": \"resetUser\",
+      \"outputs\": [],
+      \"payable\": false,
+      \"stateMutability\": \"nonpayable\",
+      \"type\": \"function\"
+    }
+  ]";
+        return $strJsonFileContents ;
+    }
+
 
 }
