612acdca887669f4f6c21f034a2da1fffae872f4
DESKTOP-QP7I888\shaban <shaban@everdreamsoft.com>
2020-02-23 21:06:44 +0100

    make adapted quantity

--- src/CsCannon/Blockchains/DataSource/CrystalSuiteDataSource.php
+++ src/CsCannon/Blockchains/DataSource/CrystalSuiteDataSource.php
@@ -22,7 +22,7 @@ use SandraCore\PdoConnexionWrapper;
  * Date: 06.06.19
  * Time: 09:53
  */
-class XchainOnBcy extends BlockchainDataSource
+class CrystalSuiteDataSource extends BlockchainDataSource
 {
 
 
@@ -36,84 +36,7 @@ class XchainOnBcy extends BlockchainDataSource
     {
 
 
-        $foreignEntityAdapter = new ForeignEntityAdapter(null,'',SandraManager::getSandra());
 
-        $blockSucker = new PdoConnexionWrapper(self::$dbHost, self::$db, self::$dbUser, self::$dbpass);
-        $sql = "SELECT sends.tx_index, b.block_time, sends.block_index, hash as tx_hash, s.address as source_address,  d.address as destination_address, a.asset as asset, `quantity`, memo FROM sends 
-JOIN index_transactions ON sends.`tx_hash_id` = index_transactions.id
-JOIN index_addresses s ON sends.`source_id` = s.id
-JOIN index_addresses d ON sends.`destination_id` = d.id
-JOIN assets a  ON sends.`asset_id` = a.id
-JOIN blocks b  ON sends.`block_index` = b.`block_index`
-         
-         LIMIT $batchMax OFFSET $offset";
-        $pdo = $blockSucker->get();
-
-        $entityArray = array();
-
-        try {
-            $pdoResult = $pdo->prepare($sql);
-            $pdoResult->execute();
-        } catch
-        (PDOException $exception) {
-
-            System::sandraException($exception);
-            return null;
-        }
-
-        $array = array();
-
-
-        $resultArray = $pdoResult->fetchAll(\PDO::FETCH_ASSOC);
-        foreach ($resultArray as $result) {
-
-            if (!($result['asset'])){
-
-                $result['asset'] ='NOASSET' ;
-                continue ;
-            }
-            //echo"asset :".$result['asset'] .PHP_EOL;
-
-
-            //add tracker
-            $trackedArray[BlockchainImporter::TRACKER_ADDRESSES] = array();
-
-            $trackedArray[BlockchainImporter::TRACKER_ADDRESSES][] = $result['source_address'] ;
-            $trackedArray[BlockchainImporter::TRACKER_ADDRESSES][] = $result['destination_address'] ;
-
-
-
-            $hash = $result['tx_hash'];
-
-            $quantity = $result['quantity'];
-            $transactionData = array("txHash" => "$hash",
-                "memo" => $result['memo'],
-                "quantity" => $quantity,
-                Blockchain::$txidConceptName => $hash,
-                BlockchainEventFactory::EVENT_SOURCE_ADDRESS => $result['source_address'],
-                BlockchainEventFactory::EVENT_DESTINATION_SIMPLE_VERB => $result['destination_address'],
-                "tokenId" => $result['asset'],
-                "blockIndex" => $result['block_index'],
-                BlockchainEventFactory::EVENT_BLOCK_TIME => $result['block_time'],
-                BlockchainImporter::TRACKER_BLOCKTIME => $result['block_time'],
-                BlockchainEventFactory::EVENT_CONTRACT => $result['asset'],
-
-
-            );
-
-            //add tracker
-            $transactionData[BlockchainImporter::TRACKER_CONTRACTIDS][] = $result['asset'] ;
-
-
-            //$transactionData[BlockchainImporter::TRACKER_BLOCKID][] =  $result['block_index'] ;
-
-
-            $transactionData = $transactionData + $trackedArray ;
-
-            $entityArray[] = $entity = new ForeignEntity($hash, $transactionData, $foreignEntityAdapter, $hash, SandraManager::getSandra());
-
-
-        }
 
         $foreignEntityAdapter->addNewEtities($entityArray,array());
 
@@ -130,7 +53,7 @@ JOIN blocks b  ON sends.`block_index` = b.`block_index`
         self::$dbHost = 'eds.alwaysdata.net';
         self::$db = 'eds2_counterparty';
         self::$dbUser = 'eds2';
-        self::$dbpass = '';
+        self::$dbpass = 'vk7B2VGmHgYuz3x';
 
 
         if ($limit)
