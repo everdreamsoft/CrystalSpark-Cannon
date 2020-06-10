72d437211d9ef10ff96f8f685f8d268d05fc7133
shaban shaame <shaban@everdreamsoft.com>
2020-06-10 08:31:50 +0200

    adding ropsten notary provider

--- src/CsCannon/Blockchains/Ethereum/DataSource/InfuraRopstenProvider.php
+++ src/CsCannon/Blockchains/Ethereum/DataSource/InfuraRopstenProvider.php
@@ -10,6 +10,9 @@ namespace CsCannon\Blockchains\Ethereum\DataSource;
 
 
 use CsCannon\Blockchains\Blockchain;
+use CsCannon\Blockchains\BlockchainContract;
+use CsCannon\Blockchains\BlockchainContractFactory;
+use CsCannon\Blockchains\BlockchainContractStandard;
 use CsCannon\Blockchains\Ethereum\EthereumBlockchain;
 use CsCannon\Blockchains\Ethereum\RopstenEthereumBlockchain;
 use CsCannon\Blockchains\RpcProvider;
@@ -25,6 +28,19 @@ class InfuraRopstenProvider extends InfuraProvider
     public function getBlockchain(): Blockchain
     {
       return  new RopstenEthereumBlockchain();
+    }
+
+    public function ownerOf(BlockchainContract $contract, $tokenId, BlockchainContractStandard $standard){
+
+
+
+        $cmd = "node CSNotary/NotaryExecutor.js --contract=".$contract->get(BlockchainContractFactory::MAIN_IDENTIFIER).
+            ' --command="ownerOf"'.
+            ' --tokenPath="{  \"tokenId\": '.$tokenId.',   \"message\": 2 }"'.
+            ' --chain="ethereum"'.
+            ' --network="ropsten"';
+        return  exec($cmd);
+
     }
 
 
