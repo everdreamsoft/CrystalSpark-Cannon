49fa5dbf5b112af4478bade877a65fff14477848
shaban shaame <shaban@everdreamsoft.com>
2020-06-16 16:07:02 +0200

    matic mor requests per sec

---
+++ src/CsCannon/Blockchains/SignableBlockchainAddress.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by EverdreamSoft.
+ * User: Shaban Shaame
+ * Date: 16.06.20
+ * Time: 16:02
+ */
+
+namespace CsCannon\Blockchains;
+
+
+class SignableBlockchainAddress
+{
+    public $address ;
+    private $privateKey ;
+    
+    public function __construct(BlockchainAddress $address,$privateKey)
+    {
+
+        $this->address = $address ;
+        $this->privateKey = $privateKey ;
+        
+    }
+
+}
