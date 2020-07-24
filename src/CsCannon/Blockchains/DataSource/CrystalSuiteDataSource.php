dc64fb39e741d23fbe4465eb44da085406f6a019
shaban shaame <shaban@everdreamsoft.com>
2020-07-24 22:35:45 +0200

    crystalsuite datasource not works with counterparty ERC721 ERC20 and xcp assets

--- src/CsCannon/Blockchains/DataSource/CrystalSuiteDataSource.php
+++ src/CsCannon/Blockchains/DataSource/CrystalSuiteDataSource.php
@@ -3,13 +3,21 @@
 namespace CsCannon\Blockchains\DataSource;
 
 use CsCannon\Balance;
+use CsCannon\BlockchainRouting;
 use CsCannon\Blockchains\Blockchain;
 use CsCannon\Blockchains\BlockchainAddress;
+use CsCannon\Blockchains\BlockchainContract;
+use CsCannon\Blockchains\BlockchainContractFactory;
+use CsCannon\Blockchains\BlockchainContractStandard;
 use CsCannon\Blockchains\BlockchainDataSource;
 use CsCannon\Blockchains\BlockchainEventFactory;
 use CsCannon\Blockchains\BlockchainImporter;
 use CsCannon\Blockchains\Counterparty\Interfaces\CounterpartyAsset;
 use CsCannon\Blockchains\Counterparty\XcpContractFactory;
+use CsCannon\Blockchains\Ethereum\Interfaces\ERC20;
+use CsCannon\Blockchains\Ethereum\Interfaces\ERC721;
+use CsCannon\Blockchains\Generic\GenericContractFactory;
+use CsCannon\Blockchains\Interfaces\UnknownStandard;
 use CsCannon\BlockchainStandardFactory;
 use CsCannon\SandraManager;
 use SandraCore\DatabaseAdapter;
@@ -52,23 +60,37 @@ class CrystalSuiteDataSource extends BlockchainDataSource
 
 
 
-        $foreignAdapter = new ForeignEntityAdapter(static::URL.'tokens/balances/'.$address->getAddress(),'data',SandraManager::getSandra());
-       // $foreignAdapter->flatSubEntity('tokens','token');
+        $foreignAdapter = new ForeignEntityAdapter(static::URL . 'tokens/balances/' . $address->getAddress(), 'data', SandraManager::getSandra());
+        // $foreignAdapter->flatSubEntity('tokens','token');
 
         $foreignAdapter->populate();
-        $me =  $foreignAdapter->divideForeignPath(['tokens'],'$first');
+        $me = $foreignAdapter->divideForeignPath(['tokens'], '$first');
 
 
         //load all counterparty contracts onto memory
-        $xcpContractFactory = new XcpContractFactory();
-        $xcpContractFactory->populateLocal();
+        $genericContractFactory = new GenericContractFactory();
+        $genericContractFactory->populateLocal();
 
         $balance = new Balance($address);
         $conterpartyAsset = CounterpartyAsset::init();
         $tokenStandardFactory = new BlockchainStandardFactory(SandraManager::getSandra());
         $tokenStandardFactory->populateLocal();
 
+        $contractIdMap =[];
+        $contractFactoryMap = [] ;
 
+        //we build a contract map
+        foreach ($foreignAdapter->foreignRawArray['data'] ?? array() as $rawContract) {
+
+            $chain = $rawContract['chain'];
+            $contractIdMap[$chain][] = $contractId = $rawContract['contract'];
+
+            if (!isset($contractFactoryMap[$chain]))
+                $contractFactoryMap[$chain] = BlockchainRouting::getBlockchainFromName($chain)->getContractFactory();
+
+        }
+
+        static::populateContractFactory($contractIdMap,$contractFactoryMap);
 
 
 
@@ -79,19 +101,162 @@ class CrystalSuiteDataSource extends BlockchainDataSource
             $chain = $rawContract['chain'];
             $tokens = $rawContract['tokens'];
 
-            $contract = $xcpContractFactory->get($contractR, true);
+
+            $blockchainStandardFactory = new BlockchainStandardFactory($foreignAdapter->system);
+            $blockchainStandardFactory->populateLocal();
+
+            $contract = $contractFactoryMap[$chain]->get($contractR);
 
 
-            if($chain != 'counterparty') continue ;
+
             foreach ($tokens ?? array() as $tokenData) {
 
 
-                $balance->addContractToken($contract, $conterpartyAsset, $tokenData['quantity']);
+                $standard = UnknownStandard::init();
+
+                //this is very bad
+                switch ($tokenData['standard']){
+                    case "ERC721":
+                        $standard = ERC721::init($tokenData['tokenId']);
+                        break ;
+                    case "Counterparty Token":
+                        $standard = CounterpartyAsset::init();
+                        break ;
+                    case "ERC20":
+                        $standard = ERC20::init();
+                        break ;
+
+                }
+
+
+                $balance->addContractToken($contract, $standard, $tokenData['quantity']);
             }
 
         }
 
         return $balance;
+
+
+    }
+
+    /**
+     * @param BlockchainAddress $address
+     * @param BlockchainContract[] $contractArray
+     * @param $limit
+     * @param $offset
+     * @return Balance
+     * @throws \Exception
+     */
+    public static  function getBalanceForContract(BlockchainAddress $address, array $contractArray, $limit, $offset):Balance
+    {
+        $keepIdList = array();
+            foreach( $contractArray as $contract){
+
+                $keepIdList[]  = $contract->getId();
+
+            }
+
+
+            //THIS IS REPETED IT SHOULD NOT !!
+            $foreignAdapter = new ForeignEntityAdapter(static::URL . 'tokens/balances/' . $address->getAddress(), 'data', SandraManager::getSandra());
+            // $foreignAdapter->flatSubEntity('tokens','token');
+
+            $foreignAdapter->populate();
+            $me = $foreignAdapter->divideForeignPath(['tokens'], '$first');
+
+
+            //load all counterparty contracts onto memory
+            $genericContractFactory = new GenericContractFactory();
+            $genericContractFactory->populateLocal();
+
+            $balance = new Balance($address);
+            $conterpartyAsset = CounterpartyAsset::init();
+            $tokenStandardFactory = new BlockchainStandardFactory(SandraManager::getSandra());
+            $tokenStandardFactory->populateLocal();
+
+            $contractIdMap =[];
+            $contractFactoryMap = [] ;
+
+            //we build a contract map
+           foreach ($foreignAdapter->foreignRawArray['data'] ?? array() as $rawContract) {
+
+               $chain = $rawContract['chain'];
+               $contractIdMap[$chain][] = $contractId = $rawContract['contract'];
+
+               if (!isset($contractFactoryMap[$chain]))
+               $contractFactoryMap[$chain] = BlockchainRouting::getBlockchainFromName($chain)->getContractFactory();
+
+            }
+
+           static::populateContractFactory($contractIdMap,$contractFactoryMap);
+
+
+
+            foreach ($foreignAdapter->foreignRawArray['data'] ?? array() as $rawContract) {
+
+                /** @var ForeignEntity $rawContract */
+                $contractR = $rawContract['contract'];
+                $chain = $rawContract['chain'];
+                $tokens = $rawContract['tokens'];
+
+
+                $blockchainStandardFactory = new BlockchainStandardFactory($foreignAdapter->system);
+                $blockchainStandardFactory->populateLocal();
+
+                $contract = $contractFactoryMap[$chain]->get($contractR);
+
+                if (!in_array($contractR,$keepIdList)){
+
+                    continue ;
+                }
+
+
+
+                foreach ($tokens ?? array() as $tokenData) {
+
+
+                    $standard = UnknownStandard::init();
+
+                    //this is very bad
+                    switch ($tokenData['standard']){
+                        case "ERC721":
+                            $standard = ERC721::init($tokenData['tokenId']);
+                            break ;
+                        case "Counterparty Token":
+                            $standard = CounterpartyAsset::init();
+                            break ;
+                        case "ERC20":
+                            $standard = ERC20::init();
+                            break ;
+
+                    }
+
+
+                    $balance->addContractToken($contract, $standard, $tokenData['quantity']);
+                }
+
+            }
+
+            return $balance;
+
+
+
+    }
+
+    /**
+     * @param array[] $contractIdMap
+     * @param BlockchainContractFactory[] $contractFactoryMap
+     */
+    private static function populateContractFactory($contractIdMap, $contractFactoryMap){
+
+        foreach ($contractFactoryMap as $chain =>$factory){
+
+            $factory->populateFromSearchResults( $contractIdMap[$chain],BlockchainContractFactory::MAIN_IDENTIFIER);
+
+        }
+
+
+
 
 
 
---
+++ tests/DataSources/CrystalSuiteDataSourceTest.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by EverdreamSoft.
+ * User: Shaban Shaame
+ * Date: 10.12.19
+ * Time: 10:17
+ */
+
+require_once __DIR__ . '/../../vendor/autoload.php'; // Autoload files using Composer autoload
+
+include 'DataSourcetest.php';
+
+
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+class CrystalSuiteDataSourceTest extends DataSourcetest
+{
+    private $contractToTest ;
+
+    public function loadTestCases() {
+
+
+       parent::loadTestCases();
+
+       $ethereumContractFactory = new \CsCannon\Blockchains\Ethereum\EthereumContractFactory();
+        //we should have 2 tokens of this contract blockchain cutties
+       $contractCuties = $ethereumContractFactory->get('0xd73be539d6b2076bab83ca6ba62dfe189abc6bbe');
+
+        //we should have 2 tokens of this contract
+        $contract = $ethereumContractFactory->get('0xf5b0a3efb8e8e4c201e2a935f110eaaf3ffecb8d');
+
+
+       $this->contractToTest[] = $contractCuties;
+       $this->contractToTest[] = $contract;
+
+
+
+    }
+
+
+    public function __construct($name = null, array $data = [], $dataName = '')
+    {
+
+
+
+        parent::__construct($name, $data, $dataName);
+    }
+
+    public function testGetBalanceForContract()
+    {
+
+        $this->loadTestCases();
+
+        $address = $this->addressToBeChecked[0];
+        $address->setDataSource(new \CsCannon\Blockchains\DataSource\CrystalSuiteDataSource());
+
+
+        $balance = $address->getBalanceForContract($this->contractToTest);
+
+        //we should have equal contract in the balance as the number of requested contracts
+        $this->assertCount(count($this->contractToTest),$balance->getContractMap());
+
+
+
+
+
+
+
+    }
+
+
+}
