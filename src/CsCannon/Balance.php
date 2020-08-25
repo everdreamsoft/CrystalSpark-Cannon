108f0bfcdfad7a34678ce67ca7da2c4e5d544929
shaban shaame <shaban@everdreamsoft.com>
2020-08-25 18:12:33 +0200

    prove with asset id

--- src/CsCannon/Balance.php
+++ src/CsCannon/Balance.php
@@ -228,6 +228,7 @@ class Balance
                 $orbDisplay['quantity'] = $quantity ;
                 $orbDisplay['adaptedQuantity'] = $refinedQuantity ;
                 $orbDisplay['asset']['image'] = $asset->imageUrl ;
+                $orbDisplay['asset']['assetId'] = $asset->id ;
 
                 //hide orbs with 0 quantity
                 if ($quantity >= 0 or $displayZeroBalance == false){
