82cd1d3444c91cf70a295d30ea59bcfbe1eaee0c
shaban shaame <shaban@everdreamsoft.com>
2021-04-16 16:00:17 +0200

    display more balance data

--- src/CsCannon/Balance.php
+++ src/CsCannon/Balance.php
@@ -242,6 +242,8 @@ class Balance
 
                 $orbDisplay['asset']['image'] = $asset->imageUrl ;
                 $orbDisplay['asset']['id'] = $asset->id ;
+                $asset->get('name') ? $orbDisplay['asset']['name'] = $asset->get('name') : null ;
+                $asset->get('description') ? $orbDisplay['asset']['description'] = $asset->get('description') : null ;
 
                 //hide orbs with 0 quantity
                 if ($quantity >= 0 or $displayZeroBalance == false){
