7cd356dad31743e93d58f7107fcf17acef5a1361
Millord <roffat.theau@outlook.com>
2022-05-17 16:48:43 +0200

    add method to XchainOnBcy for missing txs

--- src/CsCannon/Blockchains/Counterparty/DataSource/XchainOnBcy.php
+++ src/CsCannon/Blockchains/Counterparty/DataSource/XchainOnBcy.php
@@ -16,6 +16,7 @@ use SandraCore\DatabaseAdapter;
 use SandraCore\ForeignEntity;
 use SandraCore\ForeignEntityAdapter;
 use SandraCore\PdoConnexionWrapper;
+use SandraCore\System;
 
 /**
  * Created by EverdreamSoft.
@@ -31,6 +32,80 @@ class XchainOnBcy extends BlockchainDataSource
     public static $dbHost, $db, $dbpass, $dbUser ;
 
 
+    public static function getEventsFromTimestamp(string $timestamp, $contract=null, $batchMax=1000, $offset=0)
+    {
+        $foreignEntityAdapter = new ForeignEntityAdapter(null,'',SandraManager::getSandra());
+
+        $blockSucker = new PdoConnexionWrapper(self::$dbHost, self::$db, self::$dbUser, self::$dbpass);
+
+        $sql = "SELECT sends.tx_index, b.block_time, sends.block_index, hash as tx_hash, s.address as source_address,  d.address as destination_address, a.asset as asset, `quantity`, memo FROM sends
+            JOIN index_transactions ON sends.`tx_hash_id` = index_transactions.id
+            JOIN index_addresses s ON sends.`source_id` = s.id
+            JOIN index_addresses d ON sends.`destination_id` = d.id
+            JOIN assets a  ON sends.`asset_id` = a.id
+            JOIN blocks b  ON sends.`block_index` = b.`block_index`
+            WHERE b.block_time > $timestamp
+            ORDER BY b.block_time
+            LIMIT $batchMax OFFSET $offset
+            ";
+
+
+        $pdo = $blockSucker->get();
+        $entityArray = array();
+
+        try{
+            $pdoResult = $pdo->prepare($sql);
+            $pdoResult->execute();
+        }catch(PDOException $exception){
+            System::sandraException($exception);
+            return $foreignEntityAdapter;
+        }
+
+        $resultArray = $pdoResult->fetchAll(\PDO::FETCH_ASSOC);
+
+        if(empty($resultArray)){
+            $foreignEntityAdapter->entityArray = [];
+            return $foreignEntityAdapter;
+        }
+
+        foreach ($resultArray as $result) {
+
+            if (!($result['asset'])){
+                $result['asset'] ='NOASSET' ;
+                continue ;
+            }
+
+            $trackedArray[BlockchainImporter::TRACKER_ADDRESSES] = array();
+            $trackedArray[BlockchainImporter::TRACKER_ADDRESSES][] = $result['source_address'] ;
+            $trackedArray[BlockchainImporter::TRACKER_ADDRESSES][] = $result['destination_address'] ;
+
+            $hash = $result['tx_hash'];
+
+            $quantity = $result['quantity'];
+            $transactionData = array("txHash" => "$hash",
+                "memo" => $result['memo'],
+                "quantity" => $quantity,
+                Blockchain::$txidConceptName => $hash,
+                BlockchainEventFactory::EVENT_SOURCE_ADDRESS => $result['source_address'],
+                BlockchainEventFactory::EVENT_DESTINATION_SIMPLE_VERB => $result['destination_address'],
+                "tokenId" => $result['asset'],
+                "blockIndex" => $result['block_index'],
+                BlockchainEventFactory::EVENT_BLOCK_TIME => $result['block_time'],
+                BlockchainImporter::TRACKER_BLOCKTIME => $result['block_time'],
+                BlockchainEventFactory::EVENT_CONTRACT => $result['asset'],
+            );
+
+            //add tracker
+            $transactionData[BlockchainImporter::TRACKER_CONTRACTIDS][] = $result['asset'] ;
+            $transactionData = $transactionData + $trackedArray ;
+
+            $entityArray[] = new ForeignEntity($hash, $transactionData, $foreignEntityAdapter, $hash, SandraManager::getSandra());
+        }
+
+        $foreignEntityAdapter->addNewEtities($entityArray,array());
+
+        return $foreignEntityAdapter ;
+    }
 
 
     public static function getEvents($contract=null,$batchMax=1000,$offset=0,$address=null):ForeignEntityAdapter
