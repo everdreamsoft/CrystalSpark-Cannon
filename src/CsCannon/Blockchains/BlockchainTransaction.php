959fdd95e550bab22c2d355459150732b5c4d5f4
Millord <roffat.theau@outlook.com>
2022-05-24 10:04:25 +0200

    add method to XchainOnBcy for missing txs

---
+++ src/CsCannon/Blockchains/BlockchainTransaction.php
@@ -0,0 +1,129 @@
+<?php
+
+
+namespace CsCannon\Blockchains;
+
+use CsCannon\BlockchainRouting;
+use CsCannon\Displayable;
+use CsCannon\DisplayManager;
+use SandraCore\Entity;
+
+class BlockchainTransaction extends Entity implements Displayable
+{
+
+    public DisplayManager $displayManager ;
+
+    const DISPLAY_TX = 'txId';
+    const DISPLAY_TIMESTAMP = 'timestamp';
+
+    const DISPLAY_BLOCKCHAIN = 'blockchain';
+    const DISPLAY_BLOCK = 'on_block';
+    const DISPLAY_EVENTS = 'joined_events';
+
+
+    public function getBlockchain():Blockchain
+    {
+        $conceptTriplets = $this->subjectConcept->getConceptTriplets();
+        $conceptId = $conceptTriplets[$this->system->systemConcept->get(BlockchainOrderFactory::ON_BLOCKCHAIN)] ?? null;
+        $lastId = end($conceptId);
+        $blockchainName = $this->system->systemConcept->getSCS($lastId);
+
+        return BlockchainRouting::getBlockchainFromName($blockchainName);
+    }
+
+
+    /**
+     * @return BlockchainEvent[]
+     */
+    public function getJoinedEvents(): array
+    {
+        return $this->getJoinedEntities(BlockchainTransactionFactory::JOINED_EVENTS);
+    }
+
+
+    /**
+     * @return BlockchainEvent[]|null
+     */
+    public function getEvents(): ?array
+    {
+        return $this->getJoinedEntities(BlockchainTransactionFactory::JOINED_EVENT);
+    }
+
+
+    /**
+     * @return BlockchainBlock|null
+     */
+    public function getBlock(): ?BlockchainBlock
+    {
+        $blocks = $this->getJoinedEntities(BlockchainTransactionFactory::EVENT_BLOCK);
+        return is_null($blocks) ? null : reset($blocks);
+    }
+
+
+    /**
+     * @return string|null
+     */
+    public function getTxId(): ?string
+    {
+        return $this->getReference(BlockchainTransactionFactory::TX_ID)->refValue ?? null;
+    }
+
+
+    /**
+     * @return string|null
+     */
+    public function getTimestamp(): ?string
+    {
+        return $this->getReference(BlockchainTransactionFactory::EVENT_BLOCK_TIME)->refValue ?? null;
+    }
+
+
+    /**
+     * @param BlockchainEvent $event
+     * @param null $refArray
+     * @param bool $autoCommit
+     * @return Entity
+     */
+    public function addEvent(BlockchainEvent $event, $refArray = null, bool $autoCommit = true): Entity
+    {
+        return $this->setBrotherEntity(BlockchainTransactionFactory::JOINED_EVENT, $event, $refArray, $autoCommit);
+    }
+
+
+    /**
+     * @param DisplayManager $display
+     * @return array
+     */
+    public function returnArray(DisplayManager $display): array
+    {
+        $blockchain = $this->getBlockchain();
+
+        $return[self::DISPLAY_TX] = $this->getTxId();
+        $return[self::DISPLAY_TIMESTAMP] = $this->getTimestamp();
+        $return[self::DISPLAY_BLOCKCHAIN] = $blockchain::NAME;
+        $return[self::DISPLAY_BLOCK] = $this->getBlock()->getId();
+
+        $events = [];
+        foreach ($this->getEvents() ?? [] as $event){
+            $eventDisplay = $event->display();
+            $events[] = $event->returnArray($eventDisplay);
+        }
+
+        $return[self::DISPLAY_EVENTS] = $events;
+
+        return $return;
+    }
+
+
+    /**
+     * @return DisplayManager
+     */
+    public function display(): DisplayManager
+    {
+        if(!isset($this->displayManager)){
+            $this->displayManager = new DisplayManager($this);
+        }
+
+        return $this->displayManager;
+    }
+}
---
+++ src/CsCannon/Blockchains/BlockchainTransactionFactory.php
@@ -0,0 +1,116 @@
+<?php
+
+
+namespace CsCannon\Blockchains;
+
+use CsCannon\Displayable;
+use CsCannon\DisplayManager;
+use CsCannon\SandraManager;
+use SandraCore\Entity;
+use SandraCore\EntityFactory;
+
+class BlockchainTransactionFactory extends EntityFactory implements Displayable
+{
+    public Blockchain $blockchain;
+
+    public static string $isa = 'blockchainTransaction';
+    public static string $file = 'blockchainTransactionFile';
+
+    protected static string $className = 'CsCannon\Blockchains\BlockchainTransaction';
+
+    // brothers
+    const ON_BLOCKCHAIN = 'onBlockchain';
+
+    // refs
+    const TX_ID = 'txHash';
+    const EVENT_BLOCK_TIME = 'timestamp';
+
+    // joined
+    const EVENT_BLOCK = 'onBlock';
+    const JOINED_EVENTS = 'joinedEvents';
+    const JOINED_EVENT = 'joinedEvent';
+
+    public function __construct(Blockchain $blockchain)
+    {
+        parent::__construct(static::$isa, static::$file, SandraManager::getSandra());
+
+        $this->generatedEntityClass = static::$className;
+        $this->blockchain = $blockchain;
+    }
+
+
+    /**
+     * @param int $limit
+     * @param int $offset
+     * @param string $asc
+     * @param null $sortByRef
+     * @param false $numberSort
+     * @return Entity[]
+     */
+    public function populateLocal($limit = 10000, $offset = 0, $asc = 'ASC', $sortByRef = null, $numberSort = false): array
+    {
+        $populated = parent::populateLocal($limit, $offset, $asc, $sortByRef, $numberSort);
+
+        $this->joinFactory(BlockchainTransactionFactory::JOINED_EVENT, $this->blockchain->getEventFactory());
+        $this->joinFactory(BlockchainTransactionFactory::EVENT_BLOCK, $this->blockchain->getBlockFactory());
+        $this->joinPopulate();
+
+        return $populated;
+    }
+
+
+    /**
+     * @param Blockchain $blockchain
+     * @param string $txId
+     * @param $timestamp
+     * @param BlockchainBlock $block
+     * @param bool $autocommit
+     * @return Entity
+     */
+    public function createTransaction(
+        Blockchain $blockchain,
+        string $txId,
+        $timestamp,
+        BlockchainBlock $block,
+        bool $autocommit = true
+    ): Entity
+    {
+        $dataArray[Blockchain::$txidConceptName] = $txId ;
+        $dataArray[self::EVENT_BLOCK_TIME] = $timestamp ;
+
+        $triplets[self::ON_BLOCKCHAIN] = $blockchain::NAME;
+        $triplets[self::EVENT_BLOCK] = $block;
+
+        return parent::createNew($dataArray, $triplets, $autocommit);
+    }
+
+
+    /**
+     * @param DisplayManager $display
+     * @return array
+     */
+    public function returnArray(DisplayManager $display): array
+    {
+        $output = [];
+
+        foreach ($this->entityArray ?? [] as $txEntity){
+            /** @var BlockchainTransaction $txEntity */
+            $output[] = $txEntity->display()->return();
+        }
+
+        return $output;
+    }
+
+
+    /**
+     * @return DisplayManager
+     */
+    public function display(): DisplayManager
+    {
+        if(!isset($this->displayManager)){
+            $this->displayManager = new DisplayManager($this);
+        }
+
+        return $this->displayManager;
+    }
+}
