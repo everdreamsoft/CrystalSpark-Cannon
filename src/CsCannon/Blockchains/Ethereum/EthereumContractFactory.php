d006aaed0aeee20625b73cffbd8870813ffefe99
allochi <allochi@gmail.com>
2023-05-19 17:52:21 +0200

    feat: add Sepolia contract factory

--- src/CsCannon/Blockchains/Ethereum/EthereumContractFactory.php
+++ src/CsCannon/Blockchains/Ethereum/EthereumContractFactory.php
@@ -8,50 +8,28 @@
 
 namespace CsCannon\Blockchains\Ethereum;
 
-
 use CsCannon\Blockchains\BlockchainContract;
 use CsCannon\Blockchains\BlockchainContractFactory;
 use CsCannon\Blockchains\BlockchainContractStandard;
 
 class EthereumContractFactory extends BlockchainContractFactory
 {
-
     public static $isa = 'ethContract';
     const ABI_VERB = 'has';
     const ABI_TARGET = 'abi';
 
-
-
-
     protected static $className = 'CsCannon\Blockchains\Ethereum\EthereumContract' ;
-
 
     public function __construct()
     {
         $this->blockchain = EthereumBlockchain::class ;
         return parent::__construct();
-
     }
 
     public function get($identifier,$autoCreate=false,BlockchainContractStandard $contractStandard = null):?BlockchainContract
     {
-
         //in ethereum we transform it lowercase
         $identifier = strtolower($identifier);
         return parent::get($identifier,$autoCreate, $contractStandard);
-
-
     }
-
-
-
-
-
-
-
-
-
-
-
-
 }
